2017692fbb37bd700a86f71e76ea8915e15c93ab
Kelly Vaden <klvaden@gmail.com>
2021-07-03 10:36:15 -0400

    Configured Tailwind CSS. Wrote Nav and Jumbotron components.

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css';
+
+function Jumbotron() {
+  return (
+        <div id='jumbo' class='h-60 bg-blue-500 m-4 p-2 rounded-lg align-middle'>
+            <h1 class='text-6xl text-white'>Find your next read on Google Books.</h1>
+        </div>
+  );
+}
+
+export default Jumbotron;
